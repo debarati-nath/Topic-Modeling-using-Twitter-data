57d3f0b08e762792f159197dacbb00ffaf73504f
debarati-nath <nathd1@myumanitoba.ca>
2021-08-31 13:22:00 -0400

    Visualization
    Topic visualization

---
+++ visualization_tweets.py
@@ -0,0 +1,261 @@
+#Creating Topic Distance Visualization using pyLDAvis
+pyLDAvis.enable_notebook()
+gensim_models.prepare(model_5_2, corpus_tweets, id2word)
+
+def format_topics_sentences(ldamodel=None, corpus=corpus_tweets, texts=tweets):
+    # Init output
+    sent_topics_df = pd.DataFrame()
+
+    # Get main topic in each document
+    for i, row_list in enumerate(ldamodel[corpus]):
+        row = row_list[0] if ldamodel.per_word_topics else row_list
+        # print(row)
+        row = sorted(row, key=lambda x: (x[1]), reverse=True)
+        # Get the Dominant topic, Perc Contribution and Keywords for each document
+        for j, (topic_num, prop_topic) in enumerate(row):
+            if j == 0:  # => dominant topic
+                wp = ldamodel.show_topic(topic_num)
+                topic_keywords = ", ".join([word for word, prop in wp])
+                sent_topics_df = sent_topics_df.append(pd.Series([int(topic_num), round(prop_topic,4), topic_keywords]), ignore_index=True)
+            else:
+                break
+    sent_topics_df.columns = ['Dominant_Topic', 'Perc_Contribution', 'Topic_Keywords']
+
+    # Add original text to the end of the output
+    contents = pd.Series(texts)
+    sent_topics_df = pd.concat([sent_topics_df, contents], axis=1)
+    return(sent_topics_df)
+
+
+df_topic_sents_keywords = format_topics_sentences(ldamodel=lda_model, corpus=corpus_tweets, texts=tweets_lemmatized)
+
+# Dsplay after formating
+df_dominant_topic = df_topic_sents_keywords.reset_index()
+df_dominant_topic.columns = ['Document_No', 'Dominant_Topic', 'Topic_Perc_Contrib', 'Keywords', 'Text']
+df_dominant_topic.head(10)
+
+#Another way to display
+# Display setting to show more characters in column
+pd.options.display.max_colwidth = 100
+
+sent_topics_sorteddf_mallet = pd.DataFrame()
+sent_topics_outdf_grpd = df_topic_sents_keywords.groupby('Dominant_Topic')
+
+for i, grp in sent_topics_outdf_grpd:
+    sent_topics_sorteddf_mallet = pd.concat([sent_topics_sorteddf_mallet,
+                                             grp.sort_values(['Perc_Contribution'], ascending=False).head(1)],
+                                            axis=0)
+
+# Reset Index
+sent_topics_sorteddf_mallet.reset_index(drop=True, inplace=True)
+
+# Formating
+sent_topics_sorteddf_mallet.columns = ['Topic_Num', "Topic_Perc_Contrib", "Keywords", "Representative Text"]
+
+# Display
+sent_topics_sorteddf_mallet.head(10)
+
+doc_lens = [len(d) for d in df_dominant_topic.Text]
+
+# Plot - Distribution of Document World Counts
+plt.figure(figsize=(16,7), dpi=160)
+plt.hist(doc_lens, bins = 1000, color='navy')
+plt.text(750, 100, "Mean   : " + str(round(np.mean(doc_lens))))
+plt.text(750,  90, "Median : " + str(round(np.median(doc_lens))))
+plt.text(750,  80, "Stdev   : " + str(round(np.std(doc_lens))))
+plt.text(750,  70, "1%ile    : " + str(round(np.quantile(doc_lens, q=0.01))))
+plt.text(750,  60, "99%ile  : " + str(round(np.quantile(doc_lens, q=0.99))))
+
+plt.gca().set(xlim=(0, 1000), ylabel='Number of Documents', xlabel='Document Word Count')
+plt.tick_params(size=16)
+plt.xticks(np.linspace(0,1000,9))
+plt.title('Distribution of Document Word Counts', fontdict=dict(size=22))
+plt.show()
+
+
+#Plot - Distribution of Document Word Counts by Dominant Topic
+import seaborn as sns
+import matplotlib.colors as mcolors
+cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]  # more colors: 'mcolors.XKCD_COLORS'
+
+fig, axes = plt.subplots(2,2,figsize=(16,14), dpi=160, sharex=True, sharey=True)
+
+for i, ax in enumerate(axes.flatten()):
+    df_dominant_topic_sub = df_dominant_topic.loc[df_dominant_topic.Dominant_Topic == i, :]
+    doc_lens = [len(d) for d in df_dominant_topic_sub.Text]
+    ax.hist(doc_lens, bins = 1000, color=cols[i])
+    ax.tick_params(axis='y', labelcolor=cols[i], color=cols[i])
+    sns.kdeplot(doc_lens, color="black", shade=False, ax=ax.twinx())
+    ax.set(xlim=(0, 1000), xlabel='Document Word Count')
+    ax.set_ylabel('Number of Documents', color=cols[i])
+    ax.set_title('Topic: '+str(i), fontdict=dict(size=16, color=cols[i]))
+
+fig.tight_layout()
+fig.subplots_adjust(top=0.90)
+plt.xticks(np.linspace(0,1000,9))
+fig.suptitle('Distribution of Document Word Counts by Dominant Topic', fontsize=22)
+plt.show()
+
+# Display - Wordcloud of Top N words in each topic
+from matplotlib import pyplot as plt
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.colors as mcolors
+
+cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]  # more colors: 'mcolors.XKCD_COLORS'
+
+cloud = WordCloud(stopwords=STOP_WORDS,
+                  background_color='white',
+                  width=2500,
+                  height=1800,
+                  max_words=10,
+                  colormap='tab10',
+                  color_func=lambda *args, **kwargs: cols[i],
+                  prefer_horizontal=1.0)
+
+topics = lda_model.show_topics(formatted=False)
+
+fig, axes = plt.subplots(2, 2, figsize=(10,10), sharex=True, sharey=True)
+
+for i, ax in enumerate(axes.flatten()):
+    fig.add_subplot(ax)
+    topic_words = dict(topics[i][1])
+    cloud.generate_from_frequencies(topic_words, max_font_size=300)
+    plt.gca().imshow(cloud)
+    plt.gca().set_title('Topic ' + str(i), fontdict=dict(size=16))
+    plt.gca().axis('off')
+
+
+plt.subplots_adjust(wspace=0, hspace=0)
+plt.axis('off')
+plt.margins(x=0, y=0)
+plt.tight_layout()
+plt.show()
+
+
+# Display - Word count and importance of topic keywords
+from collections import Counter
+topics = lda_model.show_topics(formatted=False)
+data_flat = [w for w_list in tweets_lemmatized for w in w_list]
+counter = Counter(data_flat)
+
+out = []
+for i, topic in topics:
+    for word, weight in topic:
+        out.append([word, i , weight, counter[word]])
+
+df = pd.DataFrame(out, columns=['word', 'topic_id', 'importance', 'word_count'])
+
+# Plot Word Count and Weights of Topic Keywords
+fig, axes = plt.subplots(2, 2, figsize=(16,10), sharey=True, dpi=160)
+cols = [color for name, color in mcolors.TABLEAU_COLORS.items()]
+for i, ax in enumerate(axes.flatten()):
+    ax.bar(x='word', height="word_count", data=df.loc[df.topic_id==i, :], color=cols[i], width=0.5, alpha=0.3, label='Word Count')
+    ax_twin = ax.twinx()
+    ax_twin.bar(x='word', height="importance", data=df.loc[df.topic_id==i, :], color=cols[i], width=0.2, label='Weights')
+    ax.set_ylabel('Word Count', color=cols[i])
+    ax_twin.set_ylim(0, 0.030); ax.set_ylim(0, 3500)
+    ax.set_title('Topic: ' + str(i), color=cols[i], fontsize=16)
+    ax.tick_params(axis='y', left=False)
+    ax.set_xticklabels(df.loc[df.topic_id==i, 'word'], rotation=30, horizontalalignment= 'right')
+    ax.legend(loc='upper left'); ax_twin.legend(loc='upper right')
+
+fig.tight_layout(w_pad=2)
+fig.suptitle('Word Count and Importance of Topic Keywords', fontsize=22, y=1.05)
+plt.show()
+
+# Sentence Coloring of N Sentences per Document
+from matplotlib.patches import Rectangle
+
+def sentences_chart(lda_model=lda_model, corpus=corpus_tweets, start = 0, end = 13):
+    corp = corpus[start:end]
+    mycolors = [color for name, color in mcolors.TABLEAU_COLORS.items()]
+
+    fig, axes = plt.subplots(end-start, 1, figsize=(20, (end-start)*0.95), dpi=160)
+    axes[0].axis('off')
+    for i, ax in enumerate(axes):
+        if i > 0:
+            corp_cur = corp[i-1]
+            topic_percs, wordid_topics, wordid_phivalues = lda_model[corp_cur]
+            word_dominanttopic = [(lda_model.id2word[wd], topic[0]) for wd, topic in wordid_topics]
+            ax.text(0.01, 0.5, "Doc " + str(i-1) + ": ", verticalalignment='center',
+                    fontsize=16, color='black', transform=ax.transAxes, fontweight=700)
+
+            # Draw Rectange
+            topic_percs_sorted = sorted(topic_percs, key=lambda x: (x[1]), reverse=True)
+            ax.add_patch(Rectangle((0.0, 0.05), 0.99, 0.90, fill=None, alpha=1,
+                                   color=mycolors[topic_percs_sorted[0][0]], linewidth=2))
+
+            word_pos = 0.06
+            for j, (word, topics) in enumerate(word_dominanttopic):
+                if j < 14:
+                    ax.text(word_pos, 0.5, word,
+                            horizontalalignment='left',
+                            verticalalignment='center',
+                            fontsize=16, color=mycolors[topics],
+                            transform=ax.transAxes, fontweight=700)
+                    word_pos += .009 * len(word)  # to move the word for the next iter
+                    ax.axis('off')
+            ax.text(word_pos, 0.5, '. . .',
+                    horizontalalignment='left',
+                    verticalalignment='center',
+                    fontsize=16, color='black',
+                    transform=ax.transAxes)
+
+    plt.subplots_adjust(wspace=0, hspace=0)
+    plt.suptitle('Sentence Topic Coloring for Documents: ' + str(start) + ' to ' + str(end-2), fontsize=22, y=0.95, fontweight=700)
+    plt.tight_layout()
+    plt.show()
+sentences_chart()
+
+# Topic Distribution by Dominant Topics
+from matplotlib.ticker import FuncFormatter
+# Plot
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10, 4), dpi=120, sharey=True)
+
+ax1.bar(x='Dominant_Topic', height='count', data=df_dominant_topic_in_each_doc, width=.5, color='firebrick')
+ax1.set_xticks(range(df_dominant_topic_in_each_doc.Dominant_Topic.unique().__len__()))
+tick_formatter = FuncFormatter(lambda x, pos: 'Topic ' + str(x)+ '\n' + df_top3words.loc[df_top3words.topic_id==x, 'words'].values[0])
+ax1.xaxis.set_major_formatter(tick_formatter)
+ax1.set_title('Number of Documents by Dominant Topic', fontdict=dict(size=10))
+ax1.set_ylabel('Number of Documents')
+ax1.set_ylim(0, 1000)
+# Topic Distribution by Topic Weights
+ax2.bar(x='index', height='count', data=df_topic_weightage_by_doc, width=.5, color='steelblue')
+ax2.set_xticks(range(df_topic_weightage_by_doc.index.unique().__len__()))
+ax2.xaxis.set_major_formatter(tick_formatter)
+ax2.set_title('Number of Documents by Topic Weightage', fontdict=dict(size=10))
+plt.show()
+
+#t-SNE Clustering of N LDA topics
+# Get topic weights and dominant topics ------------
+from sklearn.manifold import TSNE
+from bokeh.plotting import figure, output_file, show
+from bokeh.models import Label
+from bokeh.io import output_notebook
+
+# Get topic weights
+topic_weights = []
+for i, row_list in enumerate(lda_model[corpus_tweets]):
+    topic_weights.append([w for i, w in row_list[0]])
+
+# Array of topic weights
+arr = pd.DataFrame(topic_weights).fillna(0).values
+
+# Keep the well separated points (optional)
+arr = arr[np.amax(arr, axis=1) > 0.35]
+
+# Dominant topic number in each doc
+topic_num = np.argmax(arr, axis=1)
+
+# tSNE Dimension Reduction
+tsne_model = TSNE(n_components=2, verbose=1, random_state=0, angle=.99, init='pca')
+tsne_lda = tsne_model.fit_transform(arr)
+
+# Plot the Topic Clusters using Bokeh
+output_notebook()
+n_topics = 4
+mycolors = np.array([color for name, color in mcolors.TABLEAU_COLORS.items()])
+plot = figure(title="t-SNE Clustering of {} LDA Topics".format(n_topics),
+              plot_width=900, plot_height=700)
+plot.scatter(x=tsne_lda[:,0], y=tsne_lda[:,1], color=mycolors[topic_num])
+show(plot)
